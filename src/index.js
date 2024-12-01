1a020b69c5c77d9d94bbd5c5882f35d13423d97f
HYJIN\isacc <isacc7224@gmail.com>
2024-12-01 23:35:11 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="react-netflix">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
